0f7e83e0c7697883e4ddb491a8df4acd40ddca9d
Kang Park <kang.park827@yahoo.com>
2018-12-22 11:55:28 -0600

    Updates

---
+++ precache-manifest.a5bb3f04133bdd1fb494f0c39c887cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "7c4441aa45833ad3ea66",
+    "url": "/ClickyGame/static/js/main.7c4441aa.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "7c4441aa45833ad3ea66",
+    "url": "/ClickyGame/static/css/main.a7ab90d3.chunk.css"
+  },
+  {
+    "revision": "e35bbe6578037c3a8d323849fdd44065",
+    "url": "/ClickyGame/index.html"
+  }
+];
